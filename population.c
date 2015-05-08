bda43872f2778bd1e1a2c88a37e3d68c89567cba
Matthieu Michels <matthieu.michels@gmail.com>
2015-05-08 13:24:47 +0200

    Issue #14: Added evaluate function call to init_population.

--- population.c
+++ population.c
@@ -7,5 +7,6 @@ void init_population(population_t *out)
     out->size = REDUCED_POPULATION_SIZE;
     for (i = 0; i < out->size; i++) {
         random_genotype(&(out->genotypes[i]));
+        evaluate(&(out->genotypes[i]));
     }
 }
